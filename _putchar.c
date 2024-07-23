4f0fc2c58195b5df6de2c6c57cc71aedd9a72403
Forget Guillaume <9430@holbertonstudents.com>
2024-07-23 15:21:21 +0200

    changed min to maj in betty style doc

--- _putchar.c
+++ _putchar.c
@@ -3,8 +3,9 @@
 /**
  * _putchar - writes the character c to stdout.
  * @c: character to print.
- * Return: on success 1.
- * on error, -1 is returned, and errno is set appropriately.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int	_putchar(char c)
